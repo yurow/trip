6a9b7ab9ff4e23ca0a65434daf99d6d2df935dde
pxie <pxie@Ctrip.com>
2016-04-09 15:06:03 +0800

    add

---
+++ HackTrip/HackTrip.Controllers/HomeContoller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace HackTrip.Controllers
+{
+    public class HomeContoller : Controller
+    {
+        public ActionResult Index()
+        {
+            return new EmptyResult();
+        }
+    }
+}
---
+++ HackTrip/HackTrip.Web/Global.asax.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace HackTrip.Web
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("*.aspx");
+            routes.IgnoreRoute("*.asmx");
+
+            routes.MapRoute(
+                "Simple", // 路由名称
+                "{controller}/{action}/{id}", // 带有参数的 URL
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // 参数默认值
+            );
+            //routes.MapRoute("Home", "{b}/{a}");
+        }
+    }
+}
