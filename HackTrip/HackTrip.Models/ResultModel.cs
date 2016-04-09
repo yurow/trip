960f14e7b6b9caccc254ebc35c0735733727a948
wangzhuo <wangzhuo@Ctrip.com>
2016-04-10 06:53:50 +0800

    add

---
+++ HackTrip/HackTrip.Models/ResultModel.cs
@@ -0,0 +1,13 @@
+﻿using HackTrip.Controllers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackTrip.Models
+{
+    public class ResultModel
+    {
+        public List<MapDistance> List { get; set; }
+    }
+}
